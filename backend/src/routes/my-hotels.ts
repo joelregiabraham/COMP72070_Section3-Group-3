0bafae18b343c3616961ef802c358e0a975c56a8
Joel Regi Abraham <Jregiabraham5574@conestogac.on.ca>
2024-02-27 23:04:30 -0500

    Created MyHotels class

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -68,4 +68,14 @@ async (req: Request, res: Response) => {
 
 });
 
+router.get("/", verifyToken, async(req: Request, res: Response)=>{
+
+  try {
+    const hotels = await Hotel.find({userID: req.userId});
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Error fectching hotel" });
+  }
+})
+
 export default router;
